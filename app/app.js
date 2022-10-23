cc949ef1da5d7c6469088607f2682fba1694f930
Toshifumi Kurosawa <kuromailserver@gmail.com>
2022-10-23 18:50:45 +0900

    campgrounds/new

--- app/app.js
+++ app/app.js
@@ -22,9 +22,9 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
-// app.get('/campgrounds/new', (req, res) => {
-//     res.render('campgrounds/new');
-// });
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
 
 // app.get('/campgrounds/:id', async (req, res) => {
 //     const campground = await Campground.findById(req.params.id);
